6e405127abcb57d501a71db44a11a58f5fe8a17b
isaias <manoelisaiasa@gmail.com>
2019-05-24 20:40:19 -0300

    caminho do .env

--- agenda.js
+++ agenda.js
@@ -1,4 +1,4 @@
-const env       = require('../../.env')
+const env       = require('./.env')
 const Telegraf  = require('telegraf')
 const Extra     = require('telegraf/extra')
 const Markup    = require('telegraf/markup')
